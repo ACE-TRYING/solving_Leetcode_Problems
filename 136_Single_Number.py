3fbeca48d7025d72752f9ee319988ef92f878302
Arnab Jana <arnabkumarjana2@gmail.com>
2025-10-19 21:36:31 +0530

    recently solved

---
+++ 136_Single_Number.py
@@ -0,0 +1,12 @@
+"""
+Question => 136:
+
+Given a non-empty array of integers nums, every element appears twice except for one.
+Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+"""
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        single_one = 2* (sum(set(nums)))-sum(nums) 
+        return single_one
